882a69e2dfd7ea55b1b9b59d305f0d5d50f0ba82
Kunte Lin <kuntelin@gmail.com>
2024-07-23 16:51:22 +0800

    add: add basic restful api

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"blockaction-api/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	blog := &routes.Blog{}
+
+	router := gin.Default()
+
+	router.GET("/blogs", blog.GetBlogs)
+	router.GET("/blogs/:id", blog.GetBlog)
+	router.POST("/blogs", blog.CreateBlog)
+	router.PUT("/blogs/:id", blog.UpdateBlog)
+	router.DELETE("/blogs/:id", blog.DeleteBlog)
+
+	router.Run(":8080")
+}
