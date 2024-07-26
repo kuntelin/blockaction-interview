c3a6d76c99472a9d511e26baf30ed6a72191186d
Kunte Lin <kuntelin@gmail.com>
2024-07-26 16:10:49 +0800

    add: add get env with default

--- common/setting.go
+++ common/setting.go
@@ -15,12 +15,19 @@ type Setting struct {
 	DATABASE_URL string
 }
 
+func getEnv(key, defaultValue string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return defaultValue
+}
+
 func GetSetting() *Setting {
 	return &Setting{
-		DEBUG:        strings.EqualFold(os.Getenv("DEBUG"), "true"),
-		PORT:         os.Getenv("PORT"),
-		LOG_LEVEL:    os.Getenv("LOG_LEVEL"),
-		LOG_OUTPUT:   os.Getenv("LOG_OUTPUT"),
-		DATABASE_URL: os.Getenv("DATABASE_URL"),
+		DEBUG:        strings.EqualFold(getEnv("DEBUG", "false"), "true"),
+		PORT:         getEnv("PORT", "8080"),
+		LOG_LEVEL:    getEnv("LOG_LEVEL", "WARNING"),
+		LOG_OUTPUT:   getEnv("LOG_OUTPUT", "ext://sys.stdout"),
+		DATABASE_URL: getEnv("DATABASE_URL", ""),
 	}
 }
